b26844cc8d8659980678ff11045361731b0a5704
yangbang <bangyang@tencent.com>
2020-07-29 02:03:05 +0800

    simplified

--- coco/__init__.py
+++
@@ -1,5 +0,0 @@
-from .pycocoevalcap.bleu.bleu import Bleu
-from .pycocoevalcap.rouge.rouge import Rouge
-from .pycocoevalcap.cider.cider import Cider
-from .pycocoevalcap.meteor.meteor import Meteor
-from .pycocoevalcap.tokenizer.ptbtokenizer import PTBTokenizer
